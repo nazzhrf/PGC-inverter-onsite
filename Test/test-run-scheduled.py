8def5970396a2410922fe5efdc9036dfba7756bd
Muhammad Arbi Minanda <arbiminanda19@gmail.com>
2023-08-07 06:56:22 +0700

    adjust test script scheduled feature

--- Test/test-run-scheduled.py
+++ Test/test-run-scheduled.py
@@ -5,6 +5,16 @@ def run_main():
     #return subprocess.Popen(['xvfb-run', '-a', '/usr/bin/python3', '/home/pi/main.py']) # run headless on terminal using xvfb
     return subprocess.Popen(['/usr/bin/python3', '/home/pi/main.py']) #run on GUI
 
+def get_pid_by_command(command):
+    try:
+        result = subprocess.check_output(['pgrep', '-f', command])
+        pid_list = result.decode('utf-8').strip().split('\n')
+        pid_list = [int(pid) for pid in pid_list]
+        pid_list.sort(reverse=True)  # Sort the PID list in descending order
+        return pid_list[0]
+    except subprocess.CalledProcessError:
+        return None
+
 def restart_main():
     main_process = run_main()
     prev_minute = (time.localtime()).tm_min
@@ -17,8 +27,8 @@ def restart_main():
         # restart main program every hour
         if (current_minute - prev_minute) >= 1: # for testing
             if main_process is not None:
-                main_process.terminate()
-                main_process.wait()  # Wait for the process to terminate gracefully
+                pid = get_pid_by_command('python3')
+                subprocess.run(["kill", "-9", str(pid)])
                 print("Main program terminated")
             main_process = run_main()
             print("Main program restarted")
